3d692236db3bd34673813cf2a3e54f1c72af8e4d
Venkatesh N <venkatesh.nandakumar@digital.ai>
2025-01-24 12:16:07 +0530

    S-108751 Including other providers in integration test

--- scripts/integtest-chart-docker.sh
+++ scripts/integtest-chart-docker.sh
@@ -6,30 +6,13 @@ if [ -z "$1" ]; then
 fi
 
 if [ -z "$2" ]; then
-    echo "Use first argument to select tests, for example: 'basic', or 'external'."
+    echo "Use second argument to select tests, for example: 'basic', or 'external'."
     exit 1
 fi
 
 if [ -z "$3" ]; then
-    echo "Use second argument to select tests 'plain', or 'openshift'"
+    echo "Use third argument to select tests 'plain', or 'openshift'"
     exit 1
-fi
-
-if [[ "$3" = "openshift" && "$4" == "withLogin" ]]; then
-    if [ -z "$REDHAT_OC_URL" ]; then
-        echo "The '\$REDHAT_OC_URL environment variable is not set."
-        exit 1
-    fi
-
-    if [ -z "$REDHAT_OC_LOGIN" ]; then
-        echo "The '\$REDHAT_OC_LOGIN environment variable is not set."
-        exit 1
-    fi
-
-    if [ -z "$REDHAT_OC_PASSWORD" ]; then
-        echo "The '\$REDHAT_OC_PASSWORD environment variable is not set."
-        exit 1
-    fi
 fi
 
 
@@ -43,7 +26,7 @@ if [ "$APP_TYPE" = "deploy" ]; then
 fi
 
 APP_OPERATOR="operator-${APP_TYPE}-${APP_TESTS}"
-OUTPUT_HOST_DIR=./build/integtest
+OUTPUT_HOST_DIR=./build/integtest/$APP_TARGET
 OUTPUT_CONTAINER_DIR="/opt/project"
 TEST_DIR="${OUTPUT_CONTAINER_DIR}/tests/e2e/${APP_OPERATOR}.yaml"
 VERSION_FILE=./xl-op/override-defaults.yaml
@@ -65,7 +48,7 @@ mkdir -p $OUTPUT_HOST_DIR/logs
 mkdir -p $OUTPUT_HOST_DIR/kube
 chmod -R 777 $OUTPUT_HOST_DIR
 
-echo "Download leatest xl-cli ${XL_CLIENT_VERSION}"
+echo "Download latest xl-cli ${XL_CLIENT_VERSION}"
 
 curl -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" \
     -o $OUTPUT_HOST_DIR/tools/xl-client-${XL_CLIENT_VERSION}-linux-amd64.bin \
@@ -81,7 +64,10 @@ yq eval ".ImageTag = \"$XL_APP_VERSION\"" $OUTPUT_HOST_DIR/tests/answers/${APP_T
 yq eval ".ImageTagRemoteRunner = \"$XL_RR_VERSION\"" $OUTPUT_HOST_DIR/tests/answers/${APP_TARGET}/*.yaml -i
 
 if [[ "$APP_TARGET" = "openshift" && "$4" == "withLogin" ]]; then
-
+    if [ -z "$REDHAT_OC_URL" || -z "$REDHAT_OC_LOGIN" || -z "$REDHAT_OC_PASSWORD" || $REDHAT_REGISTRY_SA || $REDHAT_REGISTRY_SA_TOKEN ]; then
+        echo "Openshift environment variables are not set. Please set REDHAT_OC_URL, REDHAT_OC_LOGIN, REDHAT_OC_PASSWORD, REDHAT_REGISTRY_SA and REDHAT_REGISTRY_SA_TOKEN."
+        exit 1
+    fi
     echo "Auth to $APP_TARGET"
 
     # uses https://catalog.redhat.com/software/containers/openshift4/ose-cli/5cd9ba3f5a13467289f4d51d?container-tabs=overview
@@ -92,6 +78,72 @@ if [[ "$APP_TARGET" = "openshift" && "$4" == "withLogin" ]]; then
         -u $(id -u):$(id -g) \
         registry.redhat.io/openshift4/ose-cli:latest \
         oc login $REDHAT_OC_URL --username $REDHAT_OC_LOGIN --password $REDHAT_OC_PASSWORD
+
+elif [[ "$APP_TARGET" = "aws" && "$4" == "withLogin" ]]; then
+    echo "Setting up for AWS"
+
+    if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" || -z "$AWS_DEFAULT_REGION" || -z "$EKS_CLUSTER_NAME" ]]; then
+        echo "AWS environment variables are not set. Please set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION and EKS_CLUSTER_NAME."
+        exit 1
+    fi
+    echo "Auth to $APP_TARGET"
+
+    docker run --rm \
+        -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+        -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+        -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+        -e KUBECONFIG=$OUTPUT_CONTAINER_DIR/kube/config \
+        -v $OUTPUT_HOST_DIR:$OUTPUT_CONTAINER_DIR:rw \
+        -u $(id -u):$(id -g) \
+        amazon/aws-cli:latest \
+        aws eks update-kubeconfig --name $EKS_CLUSTER_NAME --region $AWS_DEFAULT_REGION
+
+elif [[ "$APP_TARGET" = "azure" && "$4" == "withLogin" ]]; then
+    echo "Setting up for Azure"
+
+    if [[ -z "$AZURE_SUBSCRIPTION_ID" || -z "$AZURE_CLIENT_ID" || -z "$AZURE_CLIENT_SECRET" || -z "$AZURE_TENANT_ID" || -z "$AKS_CLUSTER_NAME" || -z "$AZURE_RESOURCE_GROUP" ]]; then
+        echo "Azure environment variables are not set. Please set AZURE_SUBSCRIPTION_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID, AKS_CLUSTER_NAME, and AZURE_RESOURCE_GROUP."
+        exit 1
+    fi
+    echo "Auth to $APP_TARGET"
+
+    docker run --rm \
+        -e AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID \
+        -e AZURE_CLIENT_ID=$AZURE_CLIENT_ID \
+        -e AZURE_CLIENT_SECRET=$AZURE_CLIENT_SECRET \
+        -e AZURE_TENANT_ID=$AZURE_TENANT_ID \
+        -e KUBECONFIG=$OUTPUT_CONTAINER_DIR/kube/config \
+        -v $OUTPUT_HOST_DIR:$OUTPUT_CONTAINER_DIR:rw \
+        -u $(id -u):$(id -g) \
+        mcr.microsoft.com/azure-cli:latest \
+        # bash -c "az login -u $AZURE_USERNAME -p $AZURE_PASSWORD && az aks get-credentials --resource-group $AZURE_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --overwrite-existing" \
+        az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID && az aks get-credentials --name $AKS_CLUSTER_NAME --resource-group $AZURE_RESOURCE_GROUP
+
+elif [[ "$APP_TARGET" = "gcp" && "$4" == "withLogin" ]]; then
+    echo "Setting up for GCP"
+
+    if [[ -z "$GCP_PROJECT" || -z "$GCP_CLUSTER_NAME" || -z "$GCP_ZONE" || -z "$GCP_SERVICE_ACCOUNT_KEY" ]]; then
+        echo "GCP environment variables are not set. Please set GCP_PROJECT, GCP_CLUSTER_NAME, GCP_ZONE, and GCP_SERVICE_ACCOUNT_KEY."
+        exit 1
+    fi
+    echo "Auth to $APP_TARGET"
+
+    docker run --rm \
+        -e CLOUDSDK_CORE_PROJECT=$GCP_PROJECT \
+        -v $GCP_SERVICE_ACCOUNT_KEY:/tmp/keyfile.json \
+        -v $OUTPUT_HOST_DIR:$OUTPUT_CONTAINER_DIR:rw \
+        -u $(id -u):$(id -g) \
+        google/cloud-sdk:latest \
+        gcloud auth activate-service-account --key-file=/tmp/keyfile.json && gcloud container clusters get-credentials $GCP_CLUSTER_NAME --zone $GCP_ZONE --project $GCP_PROJECT
+
+elif [[ "$APP_TARGET" = "gcp" && "$4" == "withLogin" ]]; then
+    echo "Setting up for GCP"
+
+    if [[ -z "$GCP_PROJECT" || -z "$GCP_CLUSTER_NAME" || -z "$GCP_ZONE" || -z "$GCP_ACCOUNT_CRED_FILE" || -z "$GCP_SERVICE_ACCOUNT_EMAIL" ]]; then
+        echo "GCP environment variables are not set. Please set GCP_PROJECT, GCP_CLUSTER_NAME, GCP_ZONE, GCP_SERVICE_ACCOUNT_KEY, and GCP_SERVICE_ACCOUNT_EMAIL."
+        exit 1
+    fi
+    echo "Auth to $APP_TARGET"
 
 elif [[ "$APP_TARGET" = "plain" && "$4" == "localhost" ]]; then
 
@@ -135,7 +187,7 @@ echo "TEST RESULT:"
 cat "$OUTPUT_HOST_DIR/logs/${APP_OPERATOR}.json"
 echo ""
 
-# check for fauilure in file
+# check for failure in file
 if grep -q '"failure"' "$OUTPUT_HOST_DIR/logs/${APP_OPERATOR}.json"; then
   echo "Tests failed"
   exit 1
