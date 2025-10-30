08dd4db84745a1b12373d907c98e8db51496a895
Venkatesh N <venkatesh.nandakumar@digital.ai>
2025-10-30 15:31:21 +0530

    S-126097 Refactor for future gradle deprecations

--- buildSrc/src/main/kotlin/util/GitUtil.kt
+++ buildSrc/src/main/kotlin/util/GitUtil.kt
@@ -1,25 +1,26 @@
 package util
 
 import org.gradle.api.Project
+import org.gradle.api.logging.Logger
 
 class GitUtil {
     companion object {
 
-        fun getCurrentBranch(project: Project): String {
-            return project.findProperty("branch") as String? ?: "master"
+        fun getCurrentBranch(branchProperty: String?): String {
+            return branchProperty ?: "master"
         }
 
-        fun getGithubProtocol(project: Project): String {
-            return project.findProperty("gitProtocol") as String? ?: "git"
+        fun getGithubProtocol(protocolProperty: String?): String {
+            return protocolProperty ?: "git"
         }
 
-        fun cloneRepository(project: Project, repositoryUrl: String, branchName: String, dest: String) {
-            ProcessUtil.executeCommand(project,
+        fun cloneRepository(logger: Logger, repositoryUrl: String, branchName: String, dest: String) {
+            ProcessUtil.executeCommand(logger,
                 "git clone --depth=1 --single-branch --branch $branchName $repositoryUrl \"$dest\"",
                 logOutput = true, throwErrorOnFailure = true)
         }
 
-        fun toGithubUrl(repositoryName: String, protocol: String = "git"): String {
+        fun toGithubUrl(repositoryName: String, protocol: String): String {
             val repo = Constants.githubRepository
             if (protocol == "git") {
                 return "git@github.com:$repo/$repositoryName.git"
--- buildSrc/src/main/kotlin/util/HelmUtil.kt
+++ buildSrc/src/main/kotlin/util/HelmUtil.kt
@@ -1,25 +1,26 @@
 package util
 
 import org.gradle.api.Project
+import org.gradle.api.logging.Logger
 import java.lang.IllegalStateException
 
 class HelmUtil {
     companion object {
 
-        fun helmRepo(project: Project, helmChartCli: String) {
-            ProcessUtil.executeCommand(project,
+        fun helmRepo(logger: Logger, helmChartCli: String) {
+            ProcessUtil.executeCommand(logger,
                 "\"$helmChartCli\" repo add bitnami-repo https://charts.bitnami.com/bitnami",
                 logOutput = true, throwErrorOnFailure = true)
         }
 
-        fun helmDeps(project: Project, helmChartCli: String, chartDir: String) {
-            ProcessUtil.executeCommand(project,
+        fun helmDeps(logger: Logger, helmChartCli: String, chartDir: String) {
+            ProcessUtil.executeCommand(logger,
                 "\"$helmChartCli\" dependency update \"$chartDir\"",
                 logOutput = true, throwErrorOnFailure = true)
         }
 
-        fun helmPackage(project: Project, helmChartCli: String, chartDir: String, targetDir: String): String {
-            val result = ProcessUtil.executeCommand(project,
+        fun helmPackage(logger: Logger, helmChartCli: String, chartDir: String, targetDir: String): String {
+            val result = ProcessUtil.executeCommand(logger,
                 "\"$helmChartCli\" package \"$chartDir\" --destination \"$targetDir\"",
                 logOutput = true, throwErrorOnFailure = true)
             val version = result.substringAfterLast("$targetDir/runner-", "")
